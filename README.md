c2526cbd8f11cc27ba8789d1767c471bb158d34b
SnubStumpy2007 <joshua192007@gmail.com>
2023-07-26 15:55:50 -0400

    update

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Content Management System
 # Sources
 https://github.com/WilliamCrownover/employee-content-management-system
 https://www.simplilearn.com/tutorials/nodejs-tutorial/nodejs-mysql
-https://github.com/jpd61/employee-trackerhttps://www.npmjs.com/package/figlet
+https://github.com/jpd61/employee-tracker
+https://www.npmjs.com/package/figlet
+https://dev.mysql.com/doc/refman/8.0/en/select.html
+https://dev.mysql.com/doc/refman/8.0/en/join.html
--- index.js
+++ index.js
@@ -2,11 +2,96 @@ const express = require('express')
 const mysql = require('mysql2')
 const figlet = require('figlet')
 const cTable = require('console.table');
+// const chalk = require('chalk');
 
-const connection = require('db')
+const connection = require('db');
+const { default: inquirer } = require('inquirer');
 
 
 const app = express()
+
+
+const prompt = () => {
+  inquirer.prompt({
+    name: 'choice',
+    type: 'list',
+    message: 'Select an option',
+    choices: [
+      'View All Employees',
+      'View All Roles',
+      'View All Departments',
+      'View All Employees By Department',
+      'View Department Budgets',
+      'Update Employee Role',
+      'Update Employee Manager',
+      'Add Employee',
+      'Add Role',
+      'Add Department',
+      'Remove Employee',
+      'Remove Role',
+      'Remove Department',
+      'Exit'
+    ]
+  })
+  .then((answers) => {
+    const {choices} = answers;
+    if (choices === 'View All Employees') {
+      viewAllEmployees();
+  }
+
+  if (choices === 'View All Departments') {
+    viewAllDepartments();
+  }
+
+  if (choices === 'View All Employees By Department') {
+      viewEmployeesByDepartment();
+  }
+
+  if (choices === 'Add Employee') {
+      addEmployee();
+  }
+
+  if (choices === 'Remove Employee') {
+      removeEmployee();
+  }
+
+  if (choices === 'Update Employee Role') {
+      updateEmployeeRole();
+  }
+
+  if (choices === 'Update Employee Manager') {
+      updateEmployeeManager();
+  }
+
+  if (choices === 'View All Roles') {
+      viewAllRoles();
+  }
+
+  if (choices === 'Add Role') {
+      addRole();
+  }
+
+  if (choices === 'Remove Role') {
+      removeRole();
+  }
+
+  if (choices === 'Add Department') {
+      addDepartment();
+  }
+
+  if (choices === 'View Department Budgets') {
+      viewDepartmentBudget();
+  }
+
+  if (choices === 'Remove Department') {
+      removeDepartment();
+  }
+
+  if (choices === 'Exit') {
+      connection.end();
+  }
+  })
+}
 
 
 
@@ -21,6 +106,20 @@ figlet("Welcome to the CMS!", function (err, data) {
   }
   console.log(data);
 });
+
+const viewAllEmployees = () => {
+  let sqlConnection =  `SELECT employee.id, 
+  employee.first_name, 
+  employee.last_name, 
+  role.title, 
+  department.department_name AS 'department', 
+  role.salary
+  FROM employee, role, department 
+  WHERE department.id = role.department_id 
+  AND role.id = employee.role_id
+  ORDER BY employee.id ASC`;
+}
+
 const viewAllDepartments = () => {
   return connection.promise().query('SELECT * FROM department');
 };
